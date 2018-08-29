52597b9bc01d72b960d3b058e9db0604d0b46280
Julie Park <jspark721@gmail.com>
2018-08-29 16:36:36 -0700

    add movies title bar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <table class="title-bar">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" width="45" src="camera.png" />
+              </td>
+              <td>
+                <h1>Movie Searcher</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+
       </div>
     );
   }
