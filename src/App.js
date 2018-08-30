f3a17ae81d95cf4acb05fabe54bd502a2ecf02e3
Julie Park <jspark721@gmail.com>
2018-08-30 15:08:08 -0700

    install jQuery

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ import MovieRow from './MovieRow.js'
 class App extends Component {
   constructor(props) {
     super(props);
-    console.log("This is my initializer");
+    this.state = {}
 
-    const movies = [
-      {id: 0, poster_src: "https://image.tmdb.org/t/p/w370_and_h556_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title: "Avengers: Infinity War", overview: "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle..." },
-      {id: 1, poster_src: "https://image.tmdb.org/t/p/w370_and_h556_bestv2/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg", title: "Deadpool 2", overview: "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life."}
-    ]
+    // const movies = [
+    //   {id: 0, poster_src: "https://image.tmdb.org/t/p/w370_and_h556_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title: "Avengers: Infinity War", overview: "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle..." },
+    //   {id: 1, poster_src: "https://image.tmdb.org/t/p/w370_and_h556_bestv2/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg", title: "Deadpool 2", overview: "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life."}
+    // ]
+    //
+    // let movieRows =[]
+    //
+    // movies.forEach((movie) => {
+    //   const movieRow = <MovieRow movie={movie} />
+    //   movieRows.push(movieRow)
+    // })
+    //
+    // this.state = { rows: movieRows }
 
-    let movieRows =[]
+    this.performSearch()
+  }
 
-    movies.forEach((movie) => {
-      const movieRow = <MovieRow movie={movie} />
-      movieRows.push(movieRow)
-    })
-
-    this.state = { rows: movieRows }
+  performSearch() {
+    console.log("Perform search using movieDB")
   }
 
   render() {
